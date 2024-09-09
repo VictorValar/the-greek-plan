9c9acb00c0b15e3a4fdca1ace3ec7da21955c778
Victor Valar <valar@victorvalar.me>
2024-09-08 21:35:54 -0300

    Conver deploy script to .sh

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define the source and destination directories
+sourceDir="/home/valar/dev/empire-of-the-romans/src"
+destDir="/mnt/c/Users/victor.santos2/Documents/Paradox Interactive/Victoria 3/mod/Empire of the Romans" 
+# Check if the source directory exists
+if [ ! -d "$sourceDir" ]; then
+    echo "Source directory does not exist: $sourceDir"
+    exit 1
+fi
+
+# Check if the destination directory exists
+if [ ! -d "$destDir" ]; then
+    echo "Destination directory does not exist: $destDir"
+    exit 1
+fi
+
+# Copy files from source to destination, overwriting existing files
+if cp -r "$sourceDir"/* "$destDir"; then
+    echo "Files copied successfully"
+else
+    echo "An error occurred while copying files"
+    exit 1
+fi
